fffb7fcc5a210ca81f6971bd7bae9a1c74dfb9ca
ezntek <easontek@xflymusic.com>
2023-03-11 21:44:33 +0800

    qoywuduyhwhnuduyonouynnouyafpduyunqwunkyd

--- src/fileio.rs
+++ src/fileio.rs
@@ -12,3 +12,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+pub fn dummy() {} // github would call this renderscript otherwise!
